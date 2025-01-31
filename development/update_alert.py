4a65c2ede8749127c0f479287566d016298031ba
Ez-e <eg@email.com>
2025-01-31 16:39:11 -0600

    test

--- development/update_alert.py
+++ development/update_alert.py
@@ -51,7 +51,13 @@ for root, dirs, files in os.walk("detections/"):
                     data += "\t\"enabled\": true\n}"
 
                 rule_id = alert['rule']['rule_id']
-                url = url + "?rule_id=" + rule_id
+                update_url = url + "?rule_id=" + rule_id
                 
-            elastic_data = requests.put(url, headers=headers, data=data).json()
+            elastic_data = requests.put(update_url, headers=headers, data=data).json()
             print(elastic_data)
+
+            for key in elastic_data:
+                if key == "status_code":
+                    if 404 == elastic_data["status_code"]:
+                        elastic_data = requests.post(url, headers=headers, data=data).json()
+                        print(elastic_data)
